c7dd0f61f99365b243c5e7a92cbd4c52590aec68
Kopal20 <kopalgangrade22@navgurukul.org>
2024-06-06 12:02:10 +0530

    modification

--- Fetch-API/Project_3/script.js
+++ Fetch-API/Project_3/script.js
@@ -1,9 +1,50 @@
-let input_Text=document.getElementById("input");
-let button=document.getElementById("button");
-let result=document.getElementsByClassName("result");
+let searchBar = document.querySelector("#searchbar");
+let searchButton = document.querySelector("#button");
+let result = document.querySelector(".result");
 
+function searchWord(word) {
+  let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let textWord = document.createElement("h1");
+      textWord.textContent = `Word: ${data[0].word}`;
+      result.appendChild(textWord);
+      textWord.style.marginLeft = "20px";
+      textWord.style.marginTop = "20px";
 
-function findWord(){
+      let phonetics= document.createElement("h2");
+      phonetics.textContent = `Phonetics: ${data[0].phonetics[1].text}`;
+      result.appendChild(phonetics);
+      phonetics.style.marginTop = "20px";
 
-    let url = 
+      let meaning = document.createElement("h2");
+      meaning.textContent = `Meaning: ${data[0].meanings[0].definitions[0].definition}`;
+      result.appendChild(meaning);
+      meaning.style.marginTop = "20px";
+
+      let synonums = document.createElement("h2");
+      synonums.textContent = `Synonums: ${data[0].meanings[0].synonyms[0]}`;
+      result.appendChild(synonums);
+      synonums.style.marginTop = "20px";
+
+      let antonyms = document.createElement("h2");
+      antonyms.textContent = `Antonyms: ${data[0].meanings[0].antonyms[0]}`;
+      result.appendChild(antonyms);
+      antonyms.style.marginTop = "20px";
+
+      result.style.display = "block";
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
+
+function handleSearch() {
+  result.innerHTML = "";
+  searchWord(searchBar.value);
+  searchButton.removeEventListener("click", handleSearch);
+}
+
+searchButton.addEventListener("click", handleSearch);
